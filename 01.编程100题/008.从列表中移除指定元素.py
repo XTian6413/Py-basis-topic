d830f2bcaabe600d7325495410c649bac6e6dccd
XTian6413 <111331101+XTian6413@users.noreply.github.com>
2022-12-01 20:19:32 +0800

    Update 008.从列表中移除指定元素.py

--- 01.编程100题/008.从列表中移除指定元素.py
+++ 01.编程100题/008.从列表中移除指定元素.py
@@ -1,3 +1,4 @@
+# 方法一: 逆向遍历
 lst = [1, 2, 3, 4, 5, 6]
 lst1 = [5, 6]
 
@@ -5,8 +6,10 @@ for i in range(len(lst1), -1, -1):
     if i in lst:
         lst.remove(i)
 print(lst)
-# lst = [1, 2, 3, 4, 5, 6]
-# lst1 = [5, 6]
-# lst = [i for i in lst if i not in lst1]
-# print(lst)
+
+# 方法二: 列表生成式+判断
+lst = [1, 2, 3, 4, 5, 6]
+lst1 = [5, 6]
+lst2 = [i for i in lst if i not in lst1]
+print(lst)
 
